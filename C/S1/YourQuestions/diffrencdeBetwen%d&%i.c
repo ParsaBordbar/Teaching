cd3cae6ca782e1c12c8a3791be10ffcbc466f809
Parsa Bordbar <parsab71@gmail.com>
2023-11-11 21:35:59 +0330

    Answer to diffrencdeBetwen %d & %i

---
+++ C/S1/YourQuestions/diffrencdeBetwen%d&%i.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){    
+    int x = -36; 
+    int y = 0x24; //This is 36 in Hexadecimal
+    printf("This is our x with d: %d \n" , x); 
+    printf("This is our x with i: %i \n", x);
+
+    printf("This is our y: %i", y);
+
+    return 0;
+}
