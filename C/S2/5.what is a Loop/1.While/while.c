acd526061f268b36c79c254bbc6f3ef88fdd71ee
Parsa Bordbar <parsab71@gmail.com>
2023-11-28 00:53:23 +0330

    OOP & Binnary tree

--- C/S2/5.what is a Loop/1.While/while.c
+++ C/S2/5.what is a Loop/1.While/while.c
@@ -5,9 +5,19 @@ int main(){
 
     int i = 0;
 // What is a loop? a loop is a code block that repeats!
+    // while (/* condition */)
+    // {
+    //     /* code */
+    // }
+    
+    printf("This is a while loop:\n");
     while (i < 10) {
     printf("This is the %d Line \n", i);
     i++;
+    }
+
+    while (i < 10) {
+    printf("This is the %d Line \n", i++); 
     }
 
     return 0;
